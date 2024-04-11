5d009052f29640c055b5b7d91f27bc3db61743c6
jingericer <165082537+jingericer@users.noreply.github.com>
2024-04-10 22:55:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
 const weatherDataElement = document.getElementById("weather-data");
-const myHeaders = new Headers();
-myHeaders.set("Accept", "application/json, text/plain");
-myHeaders.set("Content-Type", "application/x-www-form-urlencoded");
-myHeaders.set("Cookie", "lang_var=en-us; ousaite_language=english; ousaite_unit_cookie_2=4; ousaite_unit_cookie_5=16; ousaite_unit_cookie_10=24; ousaite_unit_cookie_3=7; ousaite_unit_cookie_4=12; ousaite_unit_cookie_1=1");
-const cookies = {
-  lang_var: "en-us",
-  ousaie_language: "english",
-  ousaie_unit_cookie_2: "4",
-  ousaie_unit_cookie_5: "16",
-  ousaie_unit_cookie_10: "24",
-  ousaie_unit_cookie_3: "7",
-  ousaie_unit_cookie_4: "12",
-  ousaie_unit_cookie_1: "1",
-};
+
 
 fetch("https://www.ecowitt.net/index/home", {
   method: "POST",
-
   body: new URLSearchParams({
     device_id: "V3ZqaWZaWFFYMGwyZ2ZtclhwV005QT09",
     authorize: "BUY4VT",
@@ -26,18 +12,20 @@ fetch("https://www.ecowitt.net/index/home", {
 .then(response => response.json())
 .then(data => {
   // Access and process weather data
-  const outdoorTemp = data.data.temp.data.tempf.value;
+  const outdoorTempFahrenheit = data.data.temp.data.tempf.value;
+  const outdoorTempCelsius = ((outdoorTempFahrenheit - 32) * (5 / 9)).toFixed(1);
   const humidity = data.data.temp.data.humidity.value;
-  const windSpeed = data.data.wind.data.windspeedmph.value;
-  const rainRate = data.data.rain.data.rainratein.value;
+  const windSpeedMph = data.data.wind.data.windspeedmph.value;
+  const windSpeedKmh = (windSpeedMph * 1.60934).toFixed(1); 
+  const rainRate = (data.data.rain.data.rainratein.value * 25.4).toFixed(1);
   const uvIndex = data.data.so_uv.data.uv.value;
   
   // Update the weather data element
   weatherDataElement.innerHTML = `
-    <p>Outdoor Temperature: ${outdoorTemp}&deg;F</p>
+    <p>Outdoor Temperature: ${outdoorTempCelsius}&deg;C</p>
     <p>Humidity: ${humidity}%</p>
-    <p>Wind Speed: ${windSpeed} mph</p>
-    <p>Rain Rate: ${rainRate} in/hr</p>
+    <p>Wind Speed: ${windSpeedKmh} km/hr</p>
+    <p>Rain Rate: ${rainRate} mm/hr</p>
     <p>UV Index: ${uvIndex}</p>
   `;
 })
